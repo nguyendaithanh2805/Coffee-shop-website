1d8f33f76ca3effcc39fde07b3cc8391c7e8d366
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-04-16 16:52:48 +0700

    feat: Add HomeController
    This commit adds a new controller class HomeController. The HomeController is responsible for handling requests related to the home page of the coffee shop website. It will serve as the entry point for users accessing the home page.

---
+++ src/main/java/org/example/coffeeshopwebsite/controller/user/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.coffeeshopwebsite.controller.user;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping({"", "/home"})
+    public String home() {
+        return "user/home";
+    }
+}
