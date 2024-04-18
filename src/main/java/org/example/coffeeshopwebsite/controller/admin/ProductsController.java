40b64b397b689206ddcc19991530af3fea0e721e
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-04-18 16:33:12 +0700

    feat: Add the 'showFormForUpdate' method for use in updating.

--- src/main/java/org/example/coffeeshopwebsite/controller/admin/ProductsController.java
+++ src/main/java/org/example/coffeeshopwebsite/controller/admin/ProductsController.java
@@ -5,16 +5,15 @@ import org.example.coffeeshopwebsite.service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -50,7 +49,6 @@ public class ProductsController {
 
             if (!(Files.exists(filePath)))
                 Files.copy(file.getInputStream(), filePath);
-
             products.setProductImage(fileName);
             productsService.saveProduct(products);
             redirectAttributes.addFlashAttribute("Message", "Image uploaded successfully");
@@ -59,5 +57,13 @@ public class ProductsController {
             redirectAttributes.addFlashAttribute("Error", "Failed to upload image");
         }
         return "redirect:/admin/products-list";
+    }
+
+
+    @GetMapping("/edit-product")
+    public String showFormForUpdate(@RequestParam Long id, Model model) {
+        Products product = productsService.getProductById(id);
+        model.addAttribute("product", product);
+        return "admin/update-product-form";
     }
 }
