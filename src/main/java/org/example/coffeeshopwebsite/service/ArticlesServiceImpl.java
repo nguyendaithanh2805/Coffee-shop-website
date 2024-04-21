90b1ed06f999d1f0d531c51799be411d1f506837
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-04-21 23:59:30 +0700

    feat: Add ArticlesService and implement.

---
+++ src/main/java/org/example/coffeeshopwebsite/service/ArticlesServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.coffeeshopwebsite.service;
+
+import org.example.coffeeshopwebsite.model.Articles;
+import org.example.coffeeshopwebsite.repository.ArticlesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticlesServiceImpl implements ArtilcesService{
+    private final ArticlesRepository articlesRepository;
+
+    @Autowired
+    public ArticlesServiceImpl(ArticlesRepository articlesRepository) {
+        this.articlesRepository = articlesRepository;
+    }
+
+    @Override
+    public List<Articles> getAllArticles() {
+        return articlesRepository.findAll();
+    }
+
+    @Override
+    public void saveArticle(Articles articles) {
+        articles.setArticleName(articles.getArticleName());
+        articles.setDescription(articles.getDescription());
+        articlesRepository.save(articles);
+    }
+}
