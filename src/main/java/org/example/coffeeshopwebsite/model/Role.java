70cd8f28bbe63fe9f98b1bf5c426b0fc1f45606b
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-05-27 15:28:35 +0700

    chore: Change the name field to 'unique = true' so that it is unique.

--- src/main/java/org/example/coffeeshopwebsite/model/Role.java
+++ src/main/java/org/example/coffeeshopwebsite/model/Role.java
@@ -11,6 +11,8 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "role")
