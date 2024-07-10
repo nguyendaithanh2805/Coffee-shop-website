c2bd0b0a21c008a094823ee009aaf4db05b49ee5
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-07-10 18:17:03 +0700

    feat: Create Category service and implement it.

---
+++ src/main/java/org/example/coffeeshopwebsite/service/CategoryService.java
@@ -0,0 +1,13 @@
+package org.example.coffeeshopwebsite.service;
+
+import org.example.coffeeshopwebsite.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    void save(Category category);
+
+    Category findCategoryById(int id);
+}
