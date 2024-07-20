abd01176b66f08fd3d79edda622a11fecf6c3dcf
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-07-20 11:58:07 +0700

    chore: Update CartController for display lisCart method.

--- src/main/java/org/example/coffeeshopwebsite/controller/CartController.java
+++ src/main/java/org/example/coffeeshopwebsite/controller/CartController.java
@@ -6,12 +6,16 @@ import org.example.coffeeshopwebsite.service.CartService;
 import org.example.coffeeshopwebsite.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/cart")
+@RequestMapping("/carts")
 public class CartController {
     private final CartService cartService;
     private final ProductService productService;
@@ -21,9 +25,14 @@ public class CartController {
         this.cartService = cartService;
         this.productService = productService;
     }
-
+    @GetMapping
+    public String listCart(Model model) {
+        List<Cart> carts = cartService.getAllProductByCart();
+        model.addAttribute("cartItems", carts);
+        return "user/carts";
+    }
     @PostMapping("/add")
-    public String addToCart(@RequestParam int productId, @RequestParam int quantity, Cart cart) {
+     public String addToCart(@RequestParam int productId, @RequestParam int quantity, Cart cart) {
         Product product = findProductByProductIdInCart(productId);
         saveCart(cart, product, productId, quantity);
         return "redirect:/menu";
